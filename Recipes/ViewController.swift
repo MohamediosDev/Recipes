5929d4a88a0ca88715bd5f524e4e5a28c9d1b1fc
mohamed Sayed <mrdiscover22@gmail.com>
2023-06-26 02:10:33 +0300

    Setup Project ⚡️
    - add Git Into Project

---
+++ Recipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipes
+//
+//  Created by Mohamed sayed on 26/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
