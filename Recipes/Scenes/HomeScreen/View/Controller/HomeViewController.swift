ec21426de71dd666ef370e26229ab22c9d79e8c1
mohamed Sayed <mrdiscover22@gmail.com>
2023-06-26 15:15:26 +0300

    feat: Add Search Controller In Home
    - Add Search Controller
    - Optimize Design And views 
    - Add Some Table View Functions

--- Recipes/Scenes/HomeScreen/View/Controller/HomeViewController.swift
+++ Recipes/Scenes/HomeScreen/View/Controller/HomeViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    //MARK: -> Outlet
     @IBOutlet weak var recipesTableView: UITableView!
+    
+    //MARK: -> Properties
+    private let searchController = UISearchController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        title = "Recipes"
+        navigationItem.searchController = searchController
     }
 
 
@@ -37,5 +47,12 @@ extension HomeViewController: UITableViewDelegate , UITableViewDataSource {
         let cell = recipesTableView.dequeueReusableCell(withIdentifier: RecioesViewCell.cellID, for: indexPath) as! RecioesViewCell
         
         return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Is Select")
     }
 }
