f56005a8adf7ded5eb7f1dc12e7a91eb225fedd2
mohamed Sayed <mrdiscover22@gmail.com>
2023-06-30 21:13:49 +0300

    Fix: Fake Repo For Unite Test

--- RecipesTests/HomeTest/FakeHomeTestRepositery.swift
+++ RecipesTests/HomeTest/FakeHomeTestRepositery.swift
@@ -11,21 +11,26 @@ import Foundation
 
 class FakeHomeRepositery: HomeRepsiteryProtocol {
     
+    
+    
     var testCaseState: ResultStates = .success
     
     func setTestCaseState(testCaseState: ResultStates) {
         self.testCaseState = testCaseState
     }
     
-    func getRecipes(complation: @escaping ([RecipesModelElement]) -> Void) {
+    func getRecipes(complation: @escaping (Result<[Recipes.RecipesModelElement], Error>) -> Void) {
         switch testCaseState {
         case .success:
-            complation([RecipesModelElement(id: "12", fats: "3", name: "Meat", time: "11:23", image: "Ss", weeks: [""], carbos: "24", fibers: "rdd", rating: 0.5, country: "EGY", ratings: 90, calories: "100", headline: "MILK", keywords: ["sss"], products: ["Sss"], proteins: "13", favorites: 09, difficulty: 10, description: "Very Goo Meat", highlighted: true, ingredients: ["sss"], deliverableIngredients: ["dd"])])
+            complation(.success([RecipesModelElement(id: "12", fats: "3", name: "Meat", time: "11:23", image: "Ss", weeks: [""], carbos: "24", fibers: "rdd", rating: 0.5, country: "EGY", ratings: 90, calories: "100", headline: "MILK", keywords: ["sss"], products: ["Sss"], proteins: "13", favorites: 09, difficulty: 10, description: "Very Goo Meat", highlighted: true, ingredients: ["sss"], deliverableIngredients: ["dd"])]))
         case .fail:
-            complation([])
+            complation(.failure(NSError(domain: "", code: 404)))
 
         }
     }
+//    func getRecipes(complation: @escaping ([RecipesModelElement]) -> Void) {
+//
+//    }
 }
 
 
