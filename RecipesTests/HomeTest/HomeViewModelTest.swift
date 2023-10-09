ca7a8375d0974eb19b920b152029a1a26a702fd0
Mohamed Abdelmaksoud <mohamed.abdelmaksoud@areebgroup.com>
2023-10-09 15:24:47 +0300

    adddd: Codecov

--- RecipesTests/HomeTest/HomeViewModelTest.swift
+++ RecipesTests/HomeTest/HomeViewModelTest.swift
@@ -23,7 +23,7 @@ final class HomeViewModelTest: XCTestCase {
     func testGetAllRecipesData_WithValidData_ShouldReturnData() {
         let expection = XCTestExpectation(description: "Get All Recipes Data")
         var recipesData: [RecipesModelElement] = []
-        print("Hello Test Uniteedd")
+        print("Hello Test Uniteeddeeee")
         viewModel.output.recipes.sink { recipes in
             recipesData = recipes
             expection.fulfill()
