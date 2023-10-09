d4802e1cd786bf64f4ba919638d755c91f6124a8
Mohamed Abdelmaksoud <mohamed.abdelmaksoud@areebgroup.com>
2023-10-09 15:34:59 +0300

    add: unitetesrt

--- RecipesTests/HomeTest/HomeViewModelTest.swift
+++ RecipesTests/HomeTest/HomeViewModelTest.swift
@@ -23,7 +23,7 @@ final class HomeViewModelTest: XCTestCase {
     func testGetAllRecipesData_WithValidData_ShouldReturnData() {
         let expection = XCTestExpectation(description: "Get All Recipes Data")
         var recipesData: [RecipesModelElement] = []
-        print("Hello Test Uniteeddeeee")
+        print("Hello Test Uniteeddeeeeeeeee")
         viewModel.output.recipes.sink { recipes in
             recipesData = recipes
             expection.fulfill()
